04ddb4abf7375e14b0731cedf215c534c8d66146
Rohithossain12 <rafidislamrohit576@gmail.com>
2025-10-04 21:30:56 +0600

    add prisma model for user, blog and project

---
+++ prisma/migrations/20251004152939_project/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "features" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "frontendUrl" TEXT,
+    "backendUrl" TEXT,
+    "thumbnail" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_slug_key" ON "Project"("slug");
